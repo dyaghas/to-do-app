f8ce1a6dea0cd96435e662b5c85f56056e84ec80
Gustavo Cristian <diamonddyaghas@gmail.com>
2022-06-20 13:36:18 -0300

    register activity added

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.todo;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,9 +17,20 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     private ArrayList<String> items;
     private ArrayAdapter<String> itemsAdapter;
@@ -30,6 +46,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference dbRef = database.getReference("message");
+
+        dbRef.setValue("Hello world!");
 
         listView = findViewById(R.id.listView);
         btnAdd = findViewById(R.id.buttonAdd);
@@ -66,6 +89,17 @@ public class MainActivity extends AppCompatActivity {
         itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         listView.setAdapter(itemsAdapter);
         setUpListViewListener();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //goes to register activity if no logged user is detected in the device
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser == null) {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
     }
 
     private void setUpListViewListener() { //method that removes an item if a long click happens
