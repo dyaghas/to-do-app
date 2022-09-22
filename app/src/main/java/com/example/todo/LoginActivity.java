79a3a69c4984a71bcb5450ac6774f9eee5a8ba94
Gustavo Cristian <diamonddyaghas@gmail.com>
2022-09-22 13:02:50 -0300

    Strings organized and empty field error added (ResetPasswordActivity).

--- app/src/main/java/com/example/todo/LoginActivity.java
+++ app/src/main/java/com/example/todo/LoginActivity.java
@@ -9,7 +9,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -47,7 +46,7 @@ public class LoginActivity extends AppCompatActivity {
         goToRegister.setOnClickListener(view ->
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class)));
         forgotPassword.setOnClickListener(view ->
-                startActivity(new Intent(LoginActivity.this, ResetPassword.class)));
+                startActivity(new Intent(LoginActivity.this, ResetPasswordActivity.class)));
 
         loginBtn = findViewById(R.id.loginBtn);
 
---
+++ app/src/main/java/com/example/todo/ResetPasswordActivity.java
@@ -0,0 +1,77 @@
+package com.example.todo;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    private TextView emailTextView;
+    private Button sendPasswordResetBtn;
+
+    private String email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+
+        emailTextView = findViewById(R.id.editTextForgotPasswordEmail);
+        sendPasswordResetBtn = findViewById(R.id.sendPasswordResetBtn);
+
+        sendPasswordResetBtn.setOnClickListener(view -> {
+            email = emailTextView.getText().toString();
+
+            //Verifies if a textview is empty or not
+            if(TextUtils.isEmpty(email)) {
+                String toastMessage = ResetPasswordActivity.this.getResources().
+                        getString(R.string.empty_fields);
+                Toast.makeText(ResetPasswordActivity.this, toastMessage,
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                resetPassword();
+            }
+        });
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        getWindow().setLayout((int) (width*.8), (int) (height*.4));
+    }
+
+    public void resetPassword() {
+        FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()) {
+                    Log.d(TAG, "resetPassword:sent");
+                    String toastMessage = ResetPasswordActivity.this.getResources()
+                            .getString(R.string.email_sent);
+                    Toast.makeText(ResetPasswordActivity.this, toastMessage,
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.w(TAG, "resetPassword:failed to send", task.getException());
+                    String toastMessage = ResetPasswordActivity.this.getResources()
+                            .getString(R.string.error);
+                    Toast.makeText(ResetPasswordActivity.this, toastMessage,
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
